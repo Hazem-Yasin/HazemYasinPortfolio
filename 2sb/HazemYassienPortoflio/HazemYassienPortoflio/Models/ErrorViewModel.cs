53908fc2823f6af086ea7a32496312f7b8b6d46c
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-05-14 01:07:05 +0300

    added test project 2

---
+++ 2sb/HazemYassienPortoflio/HazemYassienPortoflio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HazemYassienPortoflio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
